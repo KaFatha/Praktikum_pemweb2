a199177fb3364f0df1d8fdc6b67335cb31a9be12
KaFatha <fathatursina@gmail.com>
2024-05-30 08:07:31 +0700

    Praktikum Laravel 3

---
+++ puskesmas-new/resources/views/admin/index.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot name="page_name">Halaman Dashboard</x-slot>
+    <x-slot name="page_content">
+    </x-slot>
+</x-layout>
---
+++ puskesmas-new/routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\KelurahanController;
+use App\Http\Controllers\PasienController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/beranda', function () {
+    return "Halaman Beranda";
+});
+
+Route::get('/salam', function () {
+    return "Selamat Datang Fatha di Laravel 11";
+});
+
+Route::get('/profile', function () {
+    return "Halamn Profile";
+});
+
+
+//Praktikum Laravel 2
+Route::get('/dashboard', [AdminController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+
+//Praktikum Laravel 3
+Route::get('/dashboard/kelurahan', [KelurahanController::class, 'index']);
+Route::get('/dashboard/pasien', [PasienController::class, 'index']);
